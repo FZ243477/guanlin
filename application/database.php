505f9428e5955da5567c56d734d94155a65ccfe5
ChenXiang0711 <1041426058@.qq.com>
2020-04-15 14:50:04 +0800

    默认

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '192.168.1.54',
+    'hostname'        => '125.124.142.51',
     // 数据库名
     'database'        => 'yph',
     // 用户名
-    'username'        => 'cx',
+    'username'        => 'yph',
     // 密码
-    'password'        => 'cx',
+    'password'        => 'yph',
     // 端口
     'hostport'        => '',
     // 连接dsn
