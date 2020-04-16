e9199399c40bac2fcf5624735f2a612d8cdb2624
ChenXiang0711 <1041426058@.qq.com>
2020-04-16 15:53:40 +0800

    88

---
+++ application/admin/model/HousesDesigner.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\admin\model;
+
+use think\Model;
+use traits\model\SoftDelete;
+
+class HousesDesigner extends Model
+{
+    use SoftDelete;
+    protected $deleteTime = 'delete_time';
+    protected $autoWriteTimestamp = true;
+
+}
