396be24ee4bc09f5ebfcf38cf7f3b6c97ce8d228
nouyafangzhou <243477505@qq.com>
2020-04-27 13:35:39 +0800

    27

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -223,7 +223,6 @@ class Order extends Base
         if (isset($state)) {
             $order_data['state'] = $state;
         }
-        dump($order_data);exit;
         $totalCount = model('order')->where($order_data)->count();
         $pageCount = ceil($totalCount / $list_row);
         $field = 'id,create_time,order_id,state,has_take,urgent_type,fname,fphone,fprovince,fcity,fdistrict,faddress,fdetailaddress,price,take_name,weight,take_phone,take_address,take_detailaddress';
